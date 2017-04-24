e893ad86e8edc0ff15db9b67c20bc00a68ec6118
Chris Langford <demilich1988@gmail.com>
2017-04-24 10:41:46 -0400

    First Program in C# Players Guide

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints out Statement
+            Console.WriteLine("I'm sorry Dave. I'm afraid I can't do that.");
+            //Asks user to enter key before closing. 
+            Console.ReadKey();
+        }
+    }
+}
